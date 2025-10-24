40d5ebed2d234098bed9b8eb485695272a6e9cbe
TECHCON PLC <techconplc@gmail.com>
2025-10-24 12:07:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6306896341-19b03",
+  "appId": "1:47037800085:web:7fe830434e99acb803059c",
+  "apiKey": "AIzaSyCRZ_5ADWiV-j9tQVrI1wtrD4dlwYJ4BSg",
+  "authDomain": "studio-6306896341-19b03.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "47037800085"
+};
